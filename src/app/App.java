f3e95d192d6d172db08b03db499b4e45ea021b2f
patricksilva1 <patricksilva5160@gmail.com>
2022-12-07 17:33:06 -0300

    Impl Bouncy Castle

--- src/app/App.java
+++ src/app/App.java
@@ -1,6 +1,14 @@
 package app;
+
+import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Security.addProvider(new BouncyCastleProvider());
+
     }
 }
