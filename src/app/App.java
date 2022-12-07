475d1e73cdf06826793a9d1b8169a76e72060b87
patricksilva1 <patricksilva5160@gmail.com>
2022-12-07 18:39:43 -0300

    CryptoHelper working fine

--- src/app/App.java
+++ src/app/App.java
@@ -1,14 +1,31 @@
 package app;
 
+import java.security.KeyPair;
 import java.security.Security;
+import java.util.Base64;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
+import cryptocurrency.CryptographyHelper;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         Security.addProvider(new BouncyCastleProvider());
+
+        KeyPair keys = CryptographyHelper.ellipticCurveCrypto();
+
+        System.out.println();
+
+        System.out.println("Private Key: " + Base64.getEncoder().encodeToString(keys.getPrivate().getEncoded()));
+
+        System.out.println();
+
+        System.out.println("Public Key: " + Base64.getEncoder().encodeToString(keys.getPublic().getEncoded()));
+
+        System.out.println();
+
+        System.out.println("Public Key Coordinates on Elliptic Curve - " + keys.getPublic().toString());
 
     }
 }
