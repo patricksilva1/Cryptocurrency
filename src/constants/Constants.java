90d60d9171edce93282e2efffebb00f03728b7b5
patricksilva1 <patricksilva5160@gmail.com>
2022-12-07 16:02:13 -0300

    Implementing constants

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -1,5 +1,17 @@
 package constants;
 
 public class Constants {
-    
+
+    private Constants() {
+
+    }
+
+    // Leading zeros
+    public static final int DIFFICULTY = 7;
+
+    // Genesis block
+    public static final String GENESIS_PREV_HASH = "0000000000000000000000000000000000000000000000000000000000000000";
+
+    // Mine reward
+    public static final Double MINE_REWARD = 6.25;
 }
