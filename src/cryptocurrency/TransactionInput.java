0b1c0e55430c2d4c6d387d648d35b09a0cc3209d
patricksilva1 <patricksilva5160@gmail.com>
2022-12-08 14:25:52 -0300

    UTXOs working

--- src/cryptocurrency/TransactionInput.java
+++ src/cryptocurrency/TransactionInput.java
@@ -1,5 +1,31 @@
 package cryptocurrency;
 
 public class TransactionInput {
-    
+
+    // Every input has an output. This id is the transactionId of the TransactionOutput.
+    private String transactionOutputId;
+
+    // This is the unspent transaction output
+    private TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public String getTransactionOutputId() {
+        return this.transactionOutputId;
+    }
+
+    public void setTransactionOutputId(String transactionOutputId) {
+        this.transactionOutputId = transactionOutputId;
+    }
+
+    public TransactionOutput getUTXO() {
+        return this.UTXO;
+    }
+
+    public void setUTXO(TransactionOutput uTXO) {
+        this.UTXO = uTXO;
+    }
+
 }
--- src/cryptocurrency/TransactionOutput.java
+++ src/cryptocurrency/TransactionOutput.java
@@ -1,5 +1,64 @@
 package cryptocurrency;
 
+import java.security.PublicKey;
+
 public class TransactionOutput {
-    
+
+    // Identifier of the transaction output (SHA-256)
+    private String id;
+
+    // Transaction id of the parent (so the transaction it was created in)
+    private String parentTrasactionId;
+
+    // The new owner of the coin
+    private PublicKey receiver;
+
+    // Amount of coins
+    private double amount;
+
+    public TransactionOutput(PublicKey receiver, double amount, String parentTransactionId) {
+        this.receiver = receiver;
+        this.amount = amount;
+        this.parentTrasactionId = parentTransactionId;
+        generateId();
+    }
+
+    private void generateId() {
+        this.id = CryptographyHelper.generateHash(receiver.toString() + Double.toString(amount) + parentTrasactionId);
+    }
+
+    public boolean isMine(PublicKey publicKey) {
+        return publicKey == receiver;
+    }
+
+    // #region Getters and Setters
+    public String getId() {
+        return id;
+    }
+ 
+    public String getParentTrasactionId() {
+        return parentTrasactionId;
+    }
+
+    public void setParentTrasactionId(String parentTrasactionId) {
+        this.parentTrasactionId = parentTrasactionId;
+    }
+
+    public PublicKey getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(PublicKey receiver) {
+        this.receiver = receiver;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    // #endregion
 }
